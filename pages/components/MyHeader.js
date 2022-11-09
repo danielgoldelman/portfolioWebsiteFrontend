99e1758968f3ea2f8c37a5fa089111fdc8188a6f
Daniel Goldelman <dgoldelman@wesleyan.edu>
2022-11-09 16:41:03 -0500

    bottom change clickable mobile

--- pages/components/MyHeader.js
+++ pages/components/MyHeader.js
@@ -18,8 +18,8 @@ const navactions = [
 export default function MyHeader() {
 	return (
 		<div className="shadow-lg bg-[#0d1117] py-10 text-white text-xl">
-			<div className="fixed top-5 bottom-10 left-[9%] xs:left-[10%] text-3xl z-0">DG</div>
-			<div className="fixed top-7 bottom-10 right-[5%] xs:right-[10%] space-x-4 md:space-x-8 text-base z-0">
+			<div className="fixed top-5 left-[9%] xs:left-[10%] text-3xl">DG</div>
+			<div className="fixed top-7 right-[5%] xs:right-[10%] space-x-4 md:space-x-8 text-base">
 				{navactions.map(({ action, id }) => {
 					return <NavButton key={id} action={action} id={id} className="" />;
 				})}
